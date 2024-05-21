722b61ecb681bb5f91a7d3edc000ca09a1c88ffc
stelatoris <porchpu@gmail.com>
2024-05-21 22:55:37 +0200

    Day 9: Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
- public static function all() : array
- {
-    return [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Software Engineer',
-            'salary' => '$80,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Data Analyst',
-            'salary' => '$60,000'
-        ]
-        ];
- }
+class Job extends Model {
+    protected $table = 'job_listings';
 
- public static function find(int $id): array
- {
-    $job = Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-
-    if(! $job) {
-        abort(404);
-    }
-
-    return $job;
- }
-
+    protected $fillable = ['title', 'salary'];
 }
