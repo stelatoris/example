4f6bae854d7d2704db7f55d15b485cf10a071560
stelatoris <porchpu@gmail.com>
2024-05-25 18:06:39 +0200

    Day 14. Paginate

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,10 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->paginate(3);
 
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => $jobs
     ]);
 });
 
